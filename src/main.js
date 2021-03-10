5c7538d9d5021a317681c4b1dfd80d9f25fce92a
tao123456789 <2413629661@qq.com>
2021-03-10 23:50:46 +0800

    axios_study

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.prototype.$axios = axios  //全局注册，使用方法：this.$axios
+Vue.use(VueAxios,axios);
 
 Vue.config.productionTip = false
 
